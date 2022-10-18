a054bd3ebd3f8bbfe70f37f1300176e1e625ea84
Sayem92 <sayemmiha123@gmail.com>
2022-10-18 11:51:55 +0600

    add basic backend data for dragon news

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
+
+
+app.get('/', (req, res) =>{
+    res.send('News API is running')
+});
+
+app.get('/news-categories', (req, res) =>{
+    res.send(categories)
+});
+
+
+app.listen(port, () => {
+    console.log("Dragon news server is running on port:", port);
+});
